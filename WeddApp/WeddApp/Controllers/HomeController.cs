5ca0686294bd019346fb55685e70baef8deaf59b
Brandon Jones <brandon.jones@ppg.com>
2016-03-16 15:46:49 -0400

    CSS

--- WeddApp/WeddApp/Controllers/HomeController.cs
+++ WeddApp/WeddApp/Controllers/HomeController.cs
@@ -13,18 +13,47 @@ namespace WeddApp.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Location()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Location and Venue Information";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult TravelInfo()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Travel and Accomidation Information";
 
             return View();
         }
+
+        public ActionResult ThingsToDo()
+        {
+            ViewBag.Message = "Things To Do";
+
+            return View();
+        }
+
+        public ActionResult BranAng()
+        {
+            ViewBag.Message = "About Us";
+
+            return View();
+        }
+
+        public ActionResult TheWedding()
+        {
+            ViewBag.Message = "The Wedding";
+
+            return View();
+        }
+
+        public ActionResult Gifts()
+        {
+            ViewBag.Message = "Gift and Registry Information";
+
+            return View();
+        }
+
     }
 }
