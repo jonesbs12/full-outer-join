4a89ed4aee600eb33028ec0f301616cb97bd4009
Brandon Jones <brandon.jones@ppg.com>
2016-03-16 14:03:43 -0400

    New Empty Site

---
+++ WeddApp/WeddApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WeddApp.MvcApplication" Language="C#" %>
---
+++ WeddApp/WeddApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WeddApp.Startup))]
+namespace WeddApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
