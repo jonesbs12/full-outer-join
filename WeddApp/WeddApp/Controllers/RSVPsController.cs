019fc06447252c98c5d427ef70598ac6828791f0
Brandon Jones <brandon.jones@ppg.com>
2016-03-17 11:41:28 -0400

    RSVP Form and Validation

--- WeddApp/WeddApp/Controllers/RSVPsController.cs
+++ WeddApp/WeddApp/Controllers/RSVPsController.cs
@@ -46,7 +46,7 @@ namespace WeddApp.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,NumAttending,Name,Accepts,Declines,NumKidsMeal,Comments")] RSVP rSVP)
+        public ActionResult Create([Bind(Include = "ID,NumAttending,Name,AcceptRegret,NumKidsMeal,Comments")] RSVP rSVP)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace WeddApp.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,NumAttending,Name,Accepts,Declines,NumKidsMeal,Comments")] RSVP rSVP)
+        public ActionResult Edit([Bind(Include = "ID,NumAttending,Name,AcceptRegret,NumKidsMeal,Comments")] RSVP rSVP)
         {
             if (ModelState.IsValid)
             {
--- WeddApp/WeddApp/Models/RSVP.cs
+++ WeddApp/WeddApp/Models/RSVP.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace WeddApp.Models
@@ -11,16 +12,25 @@ namespace WeddApp.Models
     {
         public int ID { get; set; }
 
+        [Required]
+        [Range(0, 10)]
+        [Display(Name = "Number of Persons Attending")]
         public int NumAttending { get; set; }
 
+        [Required(ErrorMessage = "Invalid Character. Use the following format: John Doe, Jane Doe, Jimmy Doe")]
+        [RegularExpression(@"^[a-zA-Z0-9'' ',\.]+$")]
+        [Display(Name = "Name of Persons Attending")]
         public string Name { get; set; }
 
-        public bool Accepts { get; set; }
+        [Display(Name = "AcceptRegret")]
+        public string AcceptRegret { get; set; }
 
-        public bool Declines { get; set; }
-
+        [Range(0, 10)]
+        [Display(Name = "Number of Kids Meals Needed(Kids Under 12)")]
         public int NumKidsMeal { get; set; }
 
+        [Display(Name = "Questions, Comments, Allergy Information")]
+        [DataType(DataType.MultilineText)]
         public string Comments { get; set; }
     }
 
