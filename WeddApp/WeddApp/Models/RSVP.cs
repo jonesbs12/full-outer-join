582cb9ec352f74040b90a6a65c1938e05855f823
jonesbs12 <jonesbs12@gmail.com>
2016-03-16 22:05:02 -0400

    rsvp model and pictures added

---
+++ WeddApp/WeddApp/Models/RSVP.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace WeddApp.Models
+{
+    public class RSVP
+    {
+        public int ID { get; set; }
+
+        public int NumAttending { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Accepts { get; set; }
+
+        public bool Declines { get; set; }
+
+        public int NumKidsMeal { get; set; }
+
+        public string Comments { get; set; }
+    }
+
+    public class RSVPDBContext : DbContext
+    {
+        public DbSet<RSVP> RSVPs { get; set; }
+    }
+}
